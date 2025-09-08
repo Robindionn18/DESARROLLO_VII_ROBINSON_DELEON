163d7295913af076b0dbc1b7a25d8e9b79832e4d
Robindionn18 <robindionn@gmail.com>
2025-09-08 18:36:26 -0500

    Progreso del Parcial

---
+++ PARCIALES/PARCIAL_1/funciones_gimnasio.php
@@ -0,0 +1,52 @@
+<?php
+
+
+// Calcular promocion
+
+function calcular_promocion($antiguedad_meses) {
+    $descuento = 0;
+    if ($antiguedad_meses > 24) {
+        $descuento = 0.20;
+    echo "Tu decuento es del 20%.<br>";
+} elseif ($antiguedad_meses >= 13 && $antiguedad_meses <= 24) {
+    $descuento = 0.12;
+    echo "Tu decuento es del 12%.<br>";
+} elseif ($antiguedad_meses >= 3 && $antiguedad_meses <= 12) {
+    $descuento = 0.08;
+    echo "Tu decuento es del 8%.<br>";
+} elseif ($antiguedad_meses < 3) {
+    echo "No aplica para descuento.<br>";
+} else {
+    echo "No aplica para descuento<br>";
+}
+return $descuento;
+echo "<br>";
+}
+
+
+// calcular seguro medico
+
+function calcular_seguro_medico($cuota_base) {
+
+    $seguro_medico_por = $cuota_base * 0.05 ;
+    $seguro_medico = $cuota_base + $seguro_medico_por;
+    return $seguro_medico;
+
+}
+
+// Calcular Cuota final 
+
+function calcular_cuota_final($cuota_base, $descuento_porcentaje, $seguro_medico) {
+    $descuento = $cuota_base * $descuento_porcentaje ;
+    $cuota_final = $cuota_base - $descuento + $seguro_medico;
+    return $cuota_final;
+
+}
+
+
+
+
+
+
+
+?>
---
+++ PARCIALES/PARCIAL_1/operaciones_cadenas.php
@@ -0,0 +1,14 @@
+<?php
+
+//Contar palabras repetidas
+
+
+$contar_palabras_repetidas = 4 ;
+
+//Capitalizar palabras
+
+$capitalizar_palabras= 5;
+
+
+
+?>
