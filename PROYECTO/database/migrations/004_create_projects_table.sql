580ee9a695266fbc17deef93c299678230dbc4a0
Robinson De León <129919910+Robindionn18@users.noreply.github.com>
2025-12-13 23:53:03 -0500

    Delete PROYECTO directory
    Se subira la forma mejorada

--- PROYECTO/database/migrations/004_create_projects_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE projects (
-id INT AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(255) NOT NULL,
-description VARCHAR(500)
-
-
-);
--- PROYECTO/database/migrations/103_create_reserve_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE reserve (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    users_name VARCHAR(100) NOT NULL,
-    book VARCHAR(100) NOT NULL,
-    reserve_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    reserve_until DATE NULL,
-    reserve_on BOOLEAN DEFAULT TRUE
-);
